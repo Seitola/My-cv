7466316b0ed0bf8b3c38a324347fa3bd26368894
Seitola <seitola@live.se>
2026-02-26 13:20:29 +0100

    Update deploy.sh

--- my-cv-vue/deploy.sh
+++ my-cv-vue/deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/https://seitola.github.io/My-cv/ main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<Seitola>/<my-cv-vue>.git main:gh-pages
+git push -f git@github.com:<Seitola>/<My-cv.git main:main
 
 cd -
